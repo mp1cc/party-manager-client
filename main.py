4858827a5293620cf92a1aee1663861165089fbe
mp1cc <matipicc6321@gmail.com>
2022-10-13 04:44:03 -0300

    primer commit con codigo robado

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+
+
+def main():    #1
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
+    while True:
+        # Capture the video frame by frame
+        ret, frame = vid.read()
+        data, bbox, straight_qrcode = detector.detectAndDecode(frame)
+        if len(data) > 0:
+            print(data)
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        # the 'q' button is set as the
+        # quitting button you may use any
+        # desired button of your choiceq
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
